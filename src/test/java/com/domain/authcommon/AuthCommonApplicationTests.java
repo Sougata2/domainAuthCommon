f14c42a385d6baded1f086ef478bc1e5f1c12f72
sougata <sougatadas@cdac.in>
2025-11-21 17:05:25 +0530

    initial commit

---
+++ src/test/java/com/domain/authcommon/AuthCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.authcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
