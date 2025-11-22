82d820aa8c650208646fbd37b9caf06f1c9b3abb
sougata <sougata19582001@gmail.com>
2025-11-23 00:09:32 +0530

    extracting username and its default role from the request and setting the auth token to security context holder.

--- src/main/java/com/domain/authcommon/filter/AuthFilter.java
+++ src/main/java/com/domain/authcommon/filter/AuthFilter.java
@@ -1,10 +1,15 @@
 package com.domain.authcommon.filter;
 
+import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,7 +20,14 @@ import java.io.IOException;
 public class AuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("AuthFilter");
+        String username = request.getHeader("X-Username");
+        if (StringUtils.isEmpty(username)) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        String defaultRole = request.getHeader("X-Role");
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.commaSeparatedStringToAuthorityList(defaultRole));
+        SecurityContextHolder.getContext().setAuthentication(auth);
         filterChain.doFilter(request, response);
     }
 }
